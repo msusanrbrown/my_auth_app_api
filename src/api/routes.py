ad52d0682d9a9551c1df210e473ef2293ea0933e
msusanrbrown <sbrow120@fau.edu>
2021-08-25 01:23:40 +0000

    first commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from argon2 import PasswordHasher
+
+ph = PasswordHasher()
+
 
 api = Blueprint('api', __name__)
 
@@ -15,4 +20,42 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend"
     }
 
+    return jsonify(response_body), 200
+
+@api.route('/register', methods=['POST'])
+def register():
+    content = request.get_json(silent=True)
+    user = User(email=content['email'], password=ph.hash(content['password']), is_active=True)
+    db.session.add(user)
+    db.session.commit()
+
+    response_body = {
+        "message": "user created"
+    }
+
+    return jsonify(response_body), 204
+
+@api.route('/login', methods=['POST'])
+def login():
+    content = request.get_json(silent=True)
+    user = User.query.filter(User.email==content["email"]).first()
+    if user is None:
+        return jsonify({"message":"Invalid email"}), 403
+
+    if not ph.verify(user.password, content["password"]):
+        return jsonify({"message":"Invalid password"}), 403
+
+    access_token = create_access_token(identity=user.id, additional_claims={"email":user.email})
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+
+@api.route('/userinfo', methods=['GET'])
+@jwt_required()
+def userinfo():
+    current_user_id = get_jwt_identity()
+    user = User.query.filter(User.id==current_user_id).first()
+
+    response_body = {
+        "message":f"Hello {user.email}!"
+    }
     return jsonify(response_body), 200
